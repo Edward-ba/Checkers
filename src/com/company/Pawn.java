d3949db90be3a15ddd3051e2edfd5330c85bf143
Edward <ebalasingham@gmail.com>
2021-01-04 21:42:21 -0800

    [pawn] more on kill code

--- src/com/company/Pawn.java
+++ src/com/company/Pawn.java
@@ -10,16 +10,21 @@ public class Pawn extends Token {
     public boolean move(Coordinates beg, Coordinates end) {
         int mult = Player.PlayerType.Black == type ? 1 : -1;
         Player.PlayerType thisPlayerType = board.grid[beg.r][beg.c].getPlayerType();
-        Player.PlayerType otherPlayerType = board.grid[beg.r][beg.c].getPlayerType() == Player.PlayerType.Black ? Player.PlayerType.Black : Player.PlayerType.White;
-        // make sure pawn cannot move backwards and col value must change by one
+        Player.PlayerType otherPlayerType = thisPlayerType == Player.PlayerType.Black ? Player.PlayerType.White : Player.PlayerType.Black;
+        // make sure pawn cannot move backwards
         if (mult*end.r > mult*beg.r)
             return false;
+
+        // simple move
         else if (Math.abs(beg.c - end.c) == 1 && Math.abs(beg.r - end.r) == 1) {
             board.grid[end.r][end.c] = board.grid[beg.r][beg.c];
             board.grid[beg.r][beg.c] = null;
             return true;
         }
+
+        // kill code
         else if (Math.abs(beg.c - end.c) == 2 && Math.abs(beg.r - end.r) == 2) {
+            // South-West
             if (beg.c < end.c &&
                     beg.r < end.r &&
                     board.grid[beg.r + 1][beg.c + 1].getPlayerType() == otherPlayerType &&
